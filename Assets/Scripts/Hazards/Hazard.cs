e85f3a7062d391c9b97ac73c142438cc723970f6
Jay Sawant <164754866+JaySawant2707@users.noreply.github.com>
2025-08-07 19:03:29 +0530

    added tut level

--- Assets/Scripts/Hazards/Hazard.cs
+++ Assets/Scripts/Hazards/Hazard.cs
@@ -2,11 +2,12 @@ using UnityEngine;
 
 public class Hazard : MonoBehaviour
 {
+    [SerializeField] int damage = 1;
     void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.CompareTag("Player"))
         {
-            other.gameObject.GetComponent<PlayerHealth>().TakeDamage(1);
+            other.gameObject.GetComponent<PlayerHealth>().TakeDamage(damage);
         }
     }
 }
